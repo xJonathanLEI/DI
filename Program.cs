8c197e7deef3c624963c52a29b3c538bb994345a
Jonathan LEI <xJonathan@outlook.com>
2020-03-02 19:41:48 +0800

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var services = new ServiceCollection();
+
+        /*
+         * Register services here so that the final output shows `True`
+         */
+
+        /* ***************************************** */
+
+        var serviceProvider = services.BuildServiceProvider();
+        var interfaceA = serviceProvider.GetRequiredService<IMyInterfaceA>();
+        var interfaceB = serviceProvider.GetRequiredService<IMyInterfaceB>();
+
+        Console.WriteLine($"Is the same instance: {interfaceA.Id == interfaceB.Id}");
+    }
+}
+
+public interface IMyInterfaceA
+{
+    int Id { get; }
+}
+public interface IMyInterfaceB
+{
+    int Id { get; }
+}
+
+public class MyClass : IMyInterfaceA, IMyInterfaceB
+{
+    public int Id { get; }
+
+    private static readonly Random rnd = new Random();
+
+    public MyClass()
+    {
+        Id = rnd.Next();
+    }
+}
